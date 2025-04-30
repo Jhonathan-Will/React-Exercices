93ac28cab62b94b56c60a40cb5f18957de33041f
Jhonathan William <jhonathanwilliam400@gmail.com>
2025-04-30 09:39:29 -0300

    Exercice 2

--- exercices/src/App.jsx
+++ exercices/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 
 import Exercice from "./components/exercice1/exercice"
-
+import Exercice2 from "./components/exercice2/exercice"
 import Exercice3 from "./components/exercice3/exercice"
 function App() {
 
@@ -10,7 +10,8 @@ function App() {
       <h1>Exercício 1</h1>  
       <Exercice/>
 
-
+      <h1>Exercício 2</h1>
+      <Exercice2/>
 
       <h1>Exercicio 3</h1>
       <Exercice3/>
--- exercices/src/components/exercice2/exercice.jsx
+++ exercices/src/components/exercice2/exercice.jsx
@@ -1,12 +1,72 @@
+import { useState } from "react";
+import { useLongPress } from "@uidotdev/usehooks";
 
+import "./exercice.css";
 
 const Exercice = () => {
+    const [count, setCount] = useState(0); 
+    const text = document.getElementById("text");
+
+    const handlePlusPress = useLongPress(
+        () => {
+            let newCount = count + 3
+            setCount(newCount)
+            text.innerHTML = newCount
+            console.log(newCount)
+        },
+        {
+          threshold: 500,
+        }
+      );
+
+    const handleMinusPress = useLongPress(
+        () => {
+            let newCount = count - 3
+            setCount(newCount)
+            text.innerHTML = newCount
+            console.log(newCount)
+        },
+        {
+          threshold: 500,
+        }
+    );
+
+    const handleCount = (value) => {
+        if(value === 0) {
+            let newCount = count + 1
+            setCount(newCount)
+            text.innerHTML = newCount
+            console.log(newCount)
+        }else if(value === 1){
+            let newCount = count + 2
+            setCount(newCount)
+            text.innerHTML = newCount
+            console.log(newCount)
+        }else if(value === 2){
+            let newCount = count - 1
+            setCount(newCount)
+            text.innerHTML = newCount
+            console.log(newCount)
+        }else{
+            let newCount = count - 2
+            setCount(newCount)
+            text.innerHTML = newCount
+            console.log(newCount)
+        }
+    }
+
     return(
         <>
             <div>
-                <h2></h2>
+                <h2 id="text">0</h2>
 
-                <button onClick={() => {}}>+1</button>
+                <button onClick={() => handleCount(0)}>+1</button>
+                <button onClick={() => handleCount(1)}>+2</button>
+                <button {... handlePlusPress}>+3</button>
+
+                <button onClick={() => handleCount(2)}>-1</button>
+                <button onClick={() => handleCount(3)}>-2</button>
+                <button {... handleMinusPress}>-3</button>
             </div>
         </>
     )
